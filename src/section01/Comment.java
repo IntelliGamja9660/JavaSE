8565eb1b3b38fc3dc2ea3094bef40847c8c53879
nwtng <nwtng@DESKTOP-G6RRJNP>
2023-11-16 21:12:21 +0900

    variable04 실수 타입 변수

--- src/section01/Comment.java
+++ src/section01/Comment.java
@@ -4,10 +4,17 @@ public class Comment {
 	public static void main(String[] args) {
 		System.out.println("Hello, Java!");
 		
-		area(10);
+		area(10);  // area 메서드 호출
 	
 	}
+	/**JavaDoc 주석
+	 * 
+	 * 원의 넓이를 구하는 메서드
+	 * 
+	 * @param r - 반지름
+	 */
 	public static void area(int r) {
+		// 원의 넓이 원주율 X 반지름 제곱
 		double area = Math.PI*Math.pow(r, 2);
 		System.out.println("원의 넓이: "+ area);
 	}
